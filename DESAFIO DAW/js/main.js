d8f20d5d98eb9d6f1a0cb1f01d1fe6441c4c4dee
HugoLZ7 <destroyermaster10@gmail.com>
2025-02-24 20:17:22 -0600

    Desafio Completed

--- DESAFIO DAW/js/main.js
+++ DESAFIO DAW/js/main.js
@@ -1,4 +1,4 @@
-var edad, peso, altura, frecuencia, imc;
+var edad, peso, altura, frecuencia, imc, frecmax;
 var nombre ;
 var apellido;
 
@@ -15,71 +15,98 @@ altura = parseFloat(prompt("Ingresar estatura del paciente:"));
 
 frecuencia = parseInt(prompt("Ingresar frecuencia actual del paciente:"));
 
+document.write("<nav class='navbar bg-body-tertiary bg-dark'>");
+document.write("<div class='container-fluid'>");
+document.write("<span class='navbar-brand mb-0 h1' style = 'color:white'>Monitor de Salud</span>");
+document.write("</div>");
+document.write("</nav>");
+
+document.write("<div class='container text-center' style ='margin-bottom: 40px'>");
+document.write('<h1>Nombre del paciente: <span style="font-size: 1em;">' + nombre + " " + apellido + '</span></h1>');
+document.write("</div>");
+
 //IMPRESION DE DATOS
-document.write("<div class = container style = 'display: flexbox'>");
-document.write("<div class = row");
+document.write("<div class='container text-center'>");
+document.write("<div class='row'>");
 
-document.write("<div class = col");
-document.write("<h1>Nombre del Paciente:</h1>" + nombre + apellido);
+if(edad <= 100){
+document.write("<div class='col' style = 'border: 2px solid black;'>");
+document.write('<h3>Edad del paciente: <span style="font-size: 1em;">' + edad + '</span></h3>');
 document.write("<img src='img/heart.png' style ='width: 50px'>");
 document.write("</div>");
+}
 
-document.write("<div class = col");
-document.write("<h1>Edad del Paciente:</h1>" + edad);
-document.write("<img src='img/heart.png' style ='width: 50px'>");
-document.write("</div>");
-
-document.write("<div class = col");
-document.write("<h1>Peso del paciente:</h1>" + peso);
+document.write("<div class='col' style = 'border: 2px solid black;'>");
+document.write('<h3>Peso del paciente: <span style="font-size: 1em;">' + peso + '</span></h3>');
 document.write("<img src='img/peso.png' style ='width: 50px'>");
 document.write("</div>");
 
-document.write("<div class = col");
-document.write("<h1>Estatura del Paciente:</h1>" + altura);
+document.write("<div class='col' style = 'border: 2px solid black;'> ");
+document.write('<h3>Altura del paciente: <span style="font-size: 1em;">' + altura + '</span></h3>');
 document.write("<img src='img/altura.png' style ='width: 50px'>");
 document.write("</div>");
-
-document.write("<div class = col");
-document.write("<h1>Frecuencia Actual del Paciente:</h1>" + frecuencia);
-document.write("<img src='img/CARDIO.png' style ='width: 50px'>");
 document.write("</div>");
-
-document.write("<div class = col>");
-document.write("<p> El índice de masa corporal (IMC) es un número que se calcula con base en el peso y la estatura de la persona. Para la mayoría de las personas, el IMC es un indicador confiable de la gordura y se usa para identificar las categorías de peso que pueden llevar a problemas de salud.</p>");
 document.write("</div>");
 
 //FORMULA IMC
-document.write("<div class = col");
+document.write("<div class='container text-center' style ='margin-top: 30px'>");
+document.write("<div class='row'>");
+
 imc = peso / Math.pow(altura, 2);
-document.write("<h3>El IMC actual es de:</h3>" + imc);
+imc = Math.round(imc,2);
+
+document.write("<div class='col' style = 'border: 2px solid black;'>");
+document.write('<h3>IMC actual: <span style="font-size: 1em;">' + imc + '</span></h3>');
+document.write("</div>");
 
 if(imc<18.5){
-    document.write("<h3>IMC=DELGADO:</h3>");
+    document.write("<div class='col' style = 'border: 2px solid black;'>");
+    document.write("<h3>DELGADO</h3>");
+    document.write("</div>");
 }
-else if(imc>=18.5 || imc<=24.9){
-    document.write("<h3>IMC=ACEPTABLE:</h3>");
+else if(imc>=18.5 && imc<=24.9){
+    document.write("<div class='col' style = 'border: 2px solid black;'>");
+    document.write("<h3>ACEPTABLE</h3>");
+    document.write("</div>");
 }
-else if(imc>=25 || imc<=29.9){
-    document.write("<h3>IMC=SOBREPESO:</h3>");
+else if(imc>=25 && imc<=29.9){
+    document.write("<div class='col' style = 'border: 2px solid black;'>");
+    document.write("<h3>SOBREPESO</h3>");
+    document.write("</div>");
 }
-else if(imc>=30 || imc<=34.9){
-    document.write("<h3>IMC=OBESIDAD BAJA:</h3>");
+else if(imc>=30 && imc<=34.9){
+    document.write("<div class='col' style = 'border: 2px solid black;'>");
+    document.write("<h3>OBESIDAD BAJA</h3>");
+    document.write("</div>");
 }
-else if(imc>=35 || imc<=39.9){
-    document.write("<h3>IMC=OBESIDAD MEDIA:</h3>");
+else if(imc>=35 && imc<=39.9){
+    document.write("<div class='col' style = 'border: 2px solid black;'>");
+    document.write("<h3>OBESIDAD MEDIA</h3>");
+    document.write("</div>");
 }
 else if(imc>=40){
-    document.write("<h3>IMC=OBESIDAD GRAVE:</h3>");
+    document.write("<div class='col' style = 'border: 2px solid black;'>");
+    document.write("<h3>OBESIDAD GRAVE</h3>");
+    document.write("</div>");
 }
+
+document.write("<div class='col' style = 'border: 2px solid black;'>");
+document.write('<h3>Frecuencia Actual: <span style="font-size: 1em;">' + frecuencia + '</span></h3>');
 document.write("</div>");
+
+frecmax = 220 - edad;
+document.write("<div class='col' style = 'border: 2px solid black;'>");
+document.write('<h3>Frecuencia Max: <span style="font-size: 1em;">' + frecmax + '</span></h3>');
 document.write("</div>");
+
 document.write("</div>");
+
 
 //TABLA DE CLASIFICACION
 with (document) {
-    write("<div class=\"container\">");
+    write("<div class=\"container\" style ='margin-top: 30px'>");
     write("<div class=\"row\">");
-    write("<table class=\"table table-primary table-striped table-hover\">");
+    write("<table class=\"table table-info table-striped table-hover\">");
     write("<thead>");
     write("<tr>");
     write("<th>IMC[PESO(KG)/TALLA2(M)</th>");
@@ -105,7 +132,7 @@ with (document) {
     write("</tr>");
     write("<tr>");
     write("<td>30.0 - 34.9</td>");
-    write("<td>BESIDAD GRADO 1</td>");
+    write("<td>OBESIDAD GRADO 1</td>");
     write("<td>OBESIDAD</td>");
     write("</tr>");
     write("<tr>");
@@ -123,5 +150,74 @@ with (document) {
     write("</div>");
     write("</div>");
 }
+//tabla de frecuencia
+with (document) {
+    write("<div class=\"container\" style ='margin-top: 30px'>");
+    write("<div class=\"row\">");
+    write("<table class=\"table table-dark table-striped-columns table-hover\">");
+    write("<thead>");
+    write("<tr>");
+    write("<th>EDAD</th>");
+    write("<th>ZONA DE OBJETIVO DE FRECUENCIA CARDIACA</th>");
+    write("<th>PROMEDIO DE FRECUENCIA CARDIACA MAXIMA</th>");
+    write("</tr>");
+    write("</thead>");
+    write("<tbody>");
+    write("<tr>");
+    write("<td>20 años </td>");
+    write("<td>100 - 170 lpm</td>");
+    write("<td>200 lpm</td>");
+    write("</tr>");
+    write("<tr>");
+    write("<td>30 años</td>");
+    write("<td>95 - 162 lpm</td>");
+    write("<td>190 lpm</td>");
+    write("</tr>");
+    write("<tr>");
+    write("<td>35 años</td>");
+    write("<td>93 - 157 lpm</td>");
+    write("<td> 185 lpm</td>");
+    write("</tr>");
+    write("<tr>");
+    write("<td>40 años</td>");
+    write("<td>90 - 153 lpm</td>");
+    write("<td>180 lpm</td>");
+    write("</tr>");
+    write("<tr>");
+    write("<td>45 años</td>");
+    write("<td>88 - 149 lpm</td>");
+    write("<td>175 lpm</td>");
+    write("</tr>");
+    write("<tr>");
+    write("<td>50 años</td>");
+    write("<td>85 - 145 lpm</td>");
+    write("<td> 170 lpm</td>");
+    write("</tr>");
+    write("<tr>");
+    write("<td>55 años</td>");
+    write("<td>83 - 140 lpm</td>");
+    write("<td> 165 lpm</td>");
+    write("</tr>");
+    write("<tr>");
+    write("<td>60 años</td>");
+    write("<td>80 - 136 lpm</td>");
+    write("<td> 160 lpm</td>");
+    write("</tr>");
+    write("<tr>");
+    write("<td>65 años</td>");
+    write("<td>78 - 132 lpm</td>");
+    write("<td> 155 lpm</td>");
+    write("</tr>");
+    write("<tr>");
+    write("<td>70 años</td>");
+    write("<td>75 - 128 lpm</td>");
+    write("<td> 150 lpm</td>");
+    write("</tr>");
+    write("</tbody>");
+    write("</table>");
+    write("</div>");
+    write("</div>");
+}
+
 
 
